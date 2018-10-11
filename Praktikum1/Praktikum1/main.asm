a2482f236a0e418a3b5470a132be4f08ba9dc5b4
Exp <theexp111@gmail.com>
2018-10-11 12:57:18 +0200

    shitty delay (prolly wrong)

--- Praktikum1/Praktikum1/main.asm
+++ Praktikum1/Praktikum1/main.asm
@@ -5,26 +5,34 @@
 ; Author : theex
 ;
 
+; Assembler Directives
 .def temp = r16 ; r16 is our temp var
+.def led = r17
 
-ldi temp, LOW(RAMEND) ; initialize low stack
-out SPL, temp
-ldi temp, HIGH(RAMEND) ; initialize high stack
-out SPH, temp
-ldi temp, 7 ; 00000111
-out DDRB, temp
+; INIT
+	ldi temp, LOW(RAMEND) ; initialize low stack
+	out SPL, temp
+	ldi temp, HIGH(RAMEND) ; initialize high stack
+	out SPH, temp
+	ldi temp, 7 ; 0b00000111
+	out DDRB, temp ; activate output of the first 3 b pins
 
-ldi temp, 1
+	ldi led, 1
 
 mainloop:
-out PORTB, temp ; load temp into led port
-lsl temp ; left shift temp
-sbrc temp, 3 ; is 4th bit set? -> reset temp
-ldi temp, 1
+	out PORTB, led ; load temp into led port
+	lsl led ; left shift temp
+	sbrc led, 3 ; is 4th bit set? -> reset temp
+	ldi led, 1
 
-rcall wait ; sleep
-rjmp mainloop
+	rcall wait ; sleep
+	rjmp mainloop
 
-wait:
-
+wait: ; delay for 1/5s -> ~25000 cycles (200ms * 0.125 MHz)
+    ldi  r18, 33
+    ldi  r19, 119
+L1: dec  r19
+    brne L1
+    dec  r18
+    brne L1
 ret
