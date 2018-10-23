7785e04033ffbc9ebbb64081e87a03d7c359c0ad
Exp <theexp111@gmail.com>
2018-10-23 17:44:21 +0200

    why does this not work

--- Praktikum4/Praktikum4/Praktikum4/semaphore.h
+++ Praktikum4/Praktikum4/Praktikum4/semaphore.h
@@ -1,21 +1,31 @@
 #pragma once
-#include <Windows.h>
+#include <mutex>
+#include <condition_variable>
 
 class Semaphore
 {
 public:
-	Semaphore(int n = 1) : value{n} {};
+	Semaphore(int n = 1) : value{n} {}
+
 private:
 	int value = 1; //max threads running at the same time
+	std::condition_variable cvar;
+	std::mutex mtx;
+
 public:
+
 	void P()
 	{
+		std::unique_lock<std::mutex> lock(mtx);
+
 		value--;
 		int startValue = value;
-		while (value <= startValue && value < 0)
+		if (value < 0)
+			cvar.wait(lock);
+		/*while (value <= startValue && value < 0)
 		{
 			Sleep(1);
-		}
+		}*/
 	}
 
 	void V()
